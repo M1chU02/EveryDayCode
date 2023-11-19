8b7d3b3511e78ed76ffdbf354243b00b1931886a
Michał Mikołajewski <m.mikolajewski02@gmail.com>
2023-11-19 20:17:04 +0100

    Word Antonym Finder

---
+++ Word-Antonym-Finder/script.js
@@ -0,0 +1,40 @@
+const API = "BKew9bocN2pS3QAAC0QHyw==Tt6fLkzbBbWzwglu";
+const resaultEl = document.getElementById("resault");
+const searchBtn = document.getElementById("searchBtn");
+searchBtn.addEventListener("click", search);
+const form = document.getElementById("form");
+form.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    search();
+  }
+});
+
+function search() {
+  let inputValue = document.getElementById("word").value;
+  if (inputValue != "") {
+    const word = inputValue;
+    let options = {
+      method: "GET",
+      headers: { "x-api-key": API },
+    };
+    let url = "https://api.api-ninjas.com/v1/thesaurus?word=" + word;
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        resaultEl.innerHTML =
+          "<ul><li>" +
+          data.antonyms[0] +
+          "</li><li>" +
+          data.antonyms[1] +
+          "</li><li>" +
+          data.antonyms[2] +
+          "</li></ul>";
+      })
+      .catch((err) => {
+        console.log(`error ${err}`);
+      });
+  } else {
+    document.getElementById("word").value = "";
+  }
+}
